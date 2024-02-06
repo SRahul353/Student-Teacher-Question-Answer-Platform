8df12d2fe1356e5f549a0abe16af1c7917e64342
Rahul Saha <saharahul353@gmail.com>
2024-02-06 21:36:44 +0600

    Add project files.

---
+++ Student  Teacher Question & Answer Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Student__Teacher_Question___Answer_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
